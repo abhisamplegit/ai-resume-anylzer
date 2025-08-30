9e6404f839616e9a6c751b2c5aa8bae7b385f915
Abhishek B Angadi <abhishekba41@gmail.com>
2025-08-30 10:15:29 +0530

    Resume feedback componenets

--- app/routes.ts
+++ app/routes.ts
@@ -4,8 +4,7 @@ export default [
     index("routes/home.tsx"),
     route('/auth', 'routes/auth.tsx'),
     route('/upload', 'routes/upload.tsx'),
-    // route('/upload', 'routes/upload.tsx'),
-    // route('/resume/:id', 'routes/resume.tsx'),
+    route('/resume/:id', 'routes/resume.tsx'),
     // route('/wipe', 'routes/wipe.tsx'),
 ] satisfies RouteConfig;
 
